36fdb811035c8b849098122a78a91e747a5a0ae7
guruprasad N <gurublr.n@gmail.com>
2025-07-01 01:03:15 +0530

    concatenatevaluepairs

--- learnFunctions.py
+++ learnFunctions.py
@@ -10,17 +10,26 @@ def multiply(a, b):
 def divide(a, b):
     return a/b
 
+def concatenate(*args):
+    total = 0
+    for value in args:
+        total+=value
+    return total
+
+def pairsPrint(**kwargs):
+    for key,value in kwargs.items():
+        print(f"key: {key} value:{value}")
 
 
 
-res = add(3,5)
-print(res)
-
-res = sub(3,5)
-print(res)
-
-res = multiply(3,5)
-print(res)
-
-res = divide(3,5)
-print(res)
+# res = add(3,5)
+# print(res)
+#
+# res = sub(3,5)
+# print(res)
+#
+# res = multiply(3,5)
+# print(res)
+#
+# res = divide(3,5)
+# print(res)
---
+++ learnImport.py
@@ -0,0 +1,9 @@
+from learnFunctions import add, concatenate, pairsPrint
+
+result1 = add(4, -5)
+print(result1)
+
+result2 = concatenate(2, 78)
+print(result2)
+
+pairsPrint(name='guruprasad', age='42', profile='kingofindianocean', occupation='software engineer')
