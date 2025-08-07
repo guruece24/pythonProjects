c23b5a7ce82405737506b532f5d09384ec96a41f
unknown <guruece24@gmail.com>
2025-08-07 06:40:43 -0700

    checking

--- classPractice.py
+++ classPractice.py
@@ -1,6 +1,19 @@
-class Student:
-    def say_hello(self):
-        print(f"my name is  ")
+import sys
 
-st = Student()
-st.say_hello()
+
+class Student:
+    def __init__(self, name, grade):
+       self.name = name
+       self.grade = grade
+
+    def display(self):
+        print(f"student {self.name} is in grade {self.grade}")
+
+s1 = Student("guru", 10)
+s2 = Student("prasad", 43)
+s3 = Student("king", 63)
+
+s1.display()
+s2.display()
+s3.display()
+
