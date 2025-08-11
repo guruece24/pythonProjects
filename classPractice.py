97742a81594f271f29d079339dc589195655292d
Guruprasad N <guruece24@gmail.com>
2025-08-11 13:57:46 -0700

    classobj

--- classPractice.py
+++ classPractice.py
@@ -1,19 +1,21 @@
 import sys
 
-
 class Student:
-    def __init__(self, name, grade):
-       self.name = name
-       self.grade = grade
+    def __init__(self, myName, myGrade):
+       self.name = myName
+       self.grade = myGrade
 
-    def display(self):
-        print(f"student {self.name} is in grade {self.grade}")
+    def display(self, myAge):
+        print(f"student {self.name} is in grade {self.grade} and age is {myAge}")
 
 s1 = Student("guru", 10)
 s2 = Student("prasad", 43)
 s3 = Student("king", 63)
 
-s1.display()
-s2.display()
-s3.display()
+print(s1.name)
+print(s1.grade)
+
+s1.display(43)
+s2.display(54)
+s3.display(34)
 
