98f19a7403bebab43e8d60cb12445a5ef79fbd60
Guruprasad N <guruece24@gmail.com>
2025-08-21 04:13:10 +0530

    map

---
+++ lambdaTest.py
@@ -0,0 +1,13 @@
+add    = lambda a,b : a+b
+sub    = lambda a,b : a-b
+mul    = lambda a,b : a*b
+div    = lambda a,b : a/b
+square = lambda a : a*a
+
+print(add(2,3))
+print(sub(2,3))
+print(mul(2,3))
+print(div(2,3))
+print(square(10))
+
+
---
+++ mapPractice.py
@@ -0,0 +1,13 @@
+def myfunc(a, b):
+  return a + b
+
+x = map(myfunc, ('apple', 'banana', 'cherry'), ('orange', 'lemon', 'pineapple'))
+
+print(x)
+#convert the map into a list, for readability:
+print(list(x))
+
+
+fruits = ['apple', 'banana', 'orange']
+result = map(lambda fruit:fruit.upper(), fruits)
+print(list(result))
